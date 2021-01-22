82c8e9428c26356055bbfa0c0a486232c5a3cbc3
dani <dani.zaghian@gmail.com>
2021-01-22 12:16:16 -0800

    adds scopes to products index

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,22 +1,10 @@
 class Api::ProductsController < ApplicationController
 
   def index
-    @products = Product.all
-    if params[:discount]
-      @products = @products.where("price < ?", 10)
-    end
-
-    if params[:search]
-      @products = @products.where("name iLIKE ? OR description iLIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
-    end
-
-    if params[:sort] == "price" && params[:sort_order] == "asc"
-      @products = @products.order(:price)
-    elsif params[:sort] == "price" && params[:sort_order] == "desc"
-      @products = @products.order(price: :desc)
-    else
-      @products = @products.order(:id)
-    end
+    @products = Product
+      .title_search(params[:search])
+      .discounted(params[:discount])
+      .sorted(params[:sort], params[:sort_order])
     render "index.json.jb"
   end
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,20 @@ class Product < ApplicationRecord
   # validates :description, length: { in: 10..500 }
   # validates_format_of :image_url, :with => %r{\.(png|jpg|jpeg)$}i, :message => "incorrect file format (must be .png, .jpg, or .jpeg)", multiline: true
 
+  scope :title_search, ->(search_terms) { where("name ILIKE ?", "%#{search_terms}%") if search_terms }
+
+  scope :discounted, ->(check_discount) { where("price < ?", 10) if check_discount }
+
+  scope :sorted, ->(sort, sort_order) {
+    if sort == "price" && sort_order == "asc"
+      order(price: :asc)
+    elsif sort == "price" && sort_order == "desc"
+      order(price: :desc)
+    else
+      order(id: :asc)
+    end
+  }
+
   def is_discounted?
     price < 10
   end

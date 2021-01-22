ba17a361f78dbc66efe009a824e6f4314af822c8
dani <dani.zaghian@gmail.com>
2021-01-22 12:13:52 -0800

    adds active record queries to work with mini capstone frontend"

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,6 +2,21 @@ class Api::ProductsController < ApplicationController
 
   def index
     @products = Product.all
+    if params[:discount]
+      @products = @products.where("price < ?", 10)
+    end
+
+    if params[:search]
+      @products = @products.where("name iLIKE ? OR description iLIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+    end
+
+    if params[:sort] == "price" && params[:sort_order] == "asc"
+      @products = @products.order(:price)
+    elsif params[:sort] == "price" && params[:sort_order] == "desc"
+      @products = @products.order(price: :desc)
+    else
+      @products = @products.order(:id)
+    end
     render "index.json.jb"
   end
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :price, numericality: { greater_than: 0 }
-  validates :description, length: { in: 10..500 }
+  # validates :description, length: { in: 10..500 }
   # validates_format_of :image_url, :with => %r{\.(png|jpg|jpeg)$}i, :message => "incorrect file format (must be .png, .jpg, or .jpeg)", multiline: true
 
   def is_discounted?
